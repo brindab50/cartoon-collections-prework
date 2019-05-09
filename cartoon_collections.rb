a6fb09db01eeceb425c8a94a5db0d378cc1713fc
brinda behal <brindab50@gmail.com>
2019-05-09 23:45:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if strings.include?(cheese_types)
-    return true
+    return false
   else 
     return nil
     
